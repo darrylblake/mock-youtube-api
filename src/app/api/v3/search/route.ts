cd21dbac1ab485d8cde4260c22eb8ad5d86213bb
Darryl Blake <darryl@darrylblake.com>
2023-04-24 11:31:22 +1200

    fix: allow cors headers

--- src/app/api/v3/search/route.ts
+++ src/app/api/v3/search/route.ts
@@ -5,6 +5,14 @@ const generateCacheKey = (q: string | null, pageToken: string | null) => {
   return slugify(`${q}${pageToken ? pageToken : ""}`);
 };
 
+const allowCorsHeaders = {
+  "Access-Control-Allow-Credentials": "true",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  "Access-Control-Allow-Headers":
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+};
+
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const queryParams = new URLSearchParams(url.search);
@@ -31,11 +39,16 @@ export async function GET(request: Request) {
         .from("youtube-api-cache")
         // @ts-ignore ???
         .upsert({ id: cacheKey, cache: JSON.stringify(data) });
-      return new Response(JSON.stringify(data), { status: 201 });
+      return new Response(JSON.stringify(data), {
+        status: 201,
+        headers: allowCorsHeaders,
+      });
     } catch (err) {
       console.error(err);
       return new Response("Something went wrong", { status: 501 });
     }
   }
-  return new Response(cache.data?.[0].cache as string);
+  return new Response(cache.data?.[0].cache as string, {
+    headers: allowCorsHeaders,
+  });
 }
